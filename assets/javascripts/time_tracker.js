e107fd66c0accbc0f3a48f07a4bd73900d13051a
Martin Babka <babka@atteq.com>
2016-01-13 16:16:06 +0100

    Fix - AJAX calls.

--- assets/javascripts/time_tracker.js
+++ assets/javascripts/time_tracker.js
@@ -4,27 +4,30 @@
 function updateElementIfChanged(id, newContent) {
     el = $('#' + id);
     if (el.innerHTML != newContent) { 
-	el.html(newContent); 
-	updateTimeTrackerAjax();
+        el.html(newContent); 
+        updateTimeTrackerAjax();
     }
 }
 function updateTimeTrackerAjax() {
-	$('.time-tracker-ajax').each(function () {
-		$(this).click(function (event) {
-			event.preventDefault();
-			$.ajax({
-				url: $(this).attr('href'),
-				success: function (data) { updateElementIfChanged('time-tracker-menu', data); }
-			});
-		});
-	});
+    $('.time-tracker-ajax').each(function () {
+        $(this).click(function (event) {
+            $.ajax({
+                url: $(this).attr('href'),
+                success: function (data) { updateElementIfChanged('time-tracker-menu', data); }
+            });
+        });
+    });
+}
+
+function contextMenuTimeTrackerItemClick(el) {
+    $.ajax({
+        url: $(el).attr('href'),
+        success: function (data) { 
+            updateElementIfChanged('time-tracker-menu', data); 
+        }
+    });
 }
 
 $(function () {
-	/*
-	$('.time-tracker-menu-update').bind('ajax:success', function (evt, data, status, xhr) {
-		$('time-tracker-menu').html(data);
-	});
-	*/
-	updateTimeTrackerAjax();
+    updateTimeTrackerAjax();
 });
